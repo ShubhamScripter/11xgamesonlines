28aa4c1352d0a33ec15f2af1b0b044dfca32792b
shubhamscripter <shubhamgoyal05263@gmail.com>
2026-07-04 10:17:00 +0530

    Remove vite-plugin-open-in-editor from admin configuration

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -1,12 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import openInEditor from "vite-plugin-open-in-editor";
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(),react(),openInEditor({
-    editor: "code" // for VS Code
-  })],
+  plugins: [tailwindcss(), react()],
   server: {
     port: 5174, 
   },
