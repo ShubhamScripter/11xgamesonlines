219de230c317f86480c673bf753b2b0168c381f5
Rakesh Kumar <makazrakesh@gmail.com>
2026-07-22 10:33:43 +0530

    lucky spin by satyam

---
+++ frontend/src/pages/luckySpin/LuckySpin.jsx
@@ -0,0 +1,246 @@
+// import goldenfortune from '../../assets/luckywheel/goldenfortune.png'
+// import spinbutton from '../../assets/luckywheel/spinbutton.png'
+import './LuckySpin.css';
+
+const LuckySpin = () => {
+    return (
+        <div className='rouletteMain pb-[450px] md:pb-[57.6388888889%]'>
+            <div className="rouletteMain_wrap pt-5 md:pt-15">
+                <div className='rouletteMain_body'>
+                    <ul class="rouletteMain_awards hidden md:block">
+                        <li class="rouletteMain_awards_item">
+                            <div>
+                                <div class="rouletteMain_awards_medals">1</div>
+                                <div class="rouletteMain_awards_prize" >
+                                    <img src="https://jiliwin.9terawolf.com/cms/babu/image/66556c95f2f60.png" />
+                                </div>
+                                <div class="rouletteMain_awards_text" >₹3,888</div>
+                            </div>
+                        </li>
+                        <li class="rouletteMain_awards_item">
+                            <div>
+                                <div class="rouletteMain_awards_medals" >2</div>
+                                <div class="rouletteMain_awards_prize" >
+                                    <img src="https://jiliwin.9terawolf.com/cms/babu/image/66556c960d172.png"/>
+                                </div>
+                                <div class="rouletteMain_awards_text" >₹888</div>
+                            </div>
+                        </li>
+                        <li class="rouletteMain_awards_item">
+                            <div>
+                                <div class="rouletteMain_awards_medals">3</div>
+                                <div class="rouletteMain_awards_prize">
+                                    <img src="https://jiliwin.9terawolf.com/cms/babu/image/66556c961c896.png" />
+                                </div>
+                                <div class="rouletteMain_awards_text" >₹688</div>
+                            </div>
+                        </li>
+                        <li class="rouletteMain_awards_item">
+                            <div>
+                                <div class="rouletteMain_awards_medals">4</div>
+                                <div class="rouletteMain_awards_prize">
+                                    <img src="https://jiliwin.9terawolf.com/cms/babu/image/66556c962fd78.png" />
+                                </div>
+                                <div class="rouletteMain_awards_text" >₹188</div>
+                            </div>
+                        </li>
+                        <li class="rouletteMain_awards_item">
+                            <div >
+                                <div class="rouletteMain_awards_medals" >5</div>
+                                <div class="rouletteMain_awards_prize" >
+                                    <img src="https://jiliwin.9terawolf.com/cms/babu/image/66556c9647657.png" />
+                                </div>
+                                <div class="rouletteMain_awards_text" >₹88</div>
+                            </div>
+                        </li>
+                        <li class="rouletteMain_awards_item">
+                            <div >
+                                <div class="rouletteMain_awards_medals" >6</div>
+                                <div class="rouletteMain_awards_prize" >
+                                    <img src="https://jiliwin.9terawolf.com/cms/babu/image/66556c9654f3c.png" />
+                                </div>
+                            <div class="rouletteMain_awards_text" >₹68</div></div>
+                        </li>
+                        <li class="rouletteMain_awards_item">
+                            <div >
+                                <div class="rouletteMain_awards_medals" >7</div>
+                                <div class="rouletteMain_awards_prize" >
+                                    <img src="https://jiliwin.9terawolf.com/cms/babu/image/66556c966252e.png" />
+                                </div>
+                                <div class="rouletteMain_awards_text" >₹28</div>
+                            </div>
+                        </li>
+                        <li class="rouletteMain_awards_item">
+                            <div >
+                                <div class="rouletteMain_awards_medals" >8</div>
+                                <div class="rouletteMain_awards_prize" >
+                                    <img src="https://jiliwin.9terawolf.com/cms/babu/image/66556c96770b9.png" />
+                                </div>
+                                <div class="rouletteMain_awards_text" >₹10</div>
+                            </div>
+                        </li>
+                    </ul>
+                    <div class="roulette-period hidden md:block">2024-03-21 - 2032-01-31</div>
+                    <div className='rouletteMain_box relative basis-[100%] md:basis-[47.6666666667%] shrink-0 grow-0 pb-[100%] md:pb-[47.6666666667%] '>
+                        <div class="roulette_main_numberTimes_left">
+                            <span class="font-bold text-[20px]">30</span>
+                            <span>coins</span>
+                            <br/>
+                            /swing
+                        </div>
+
+
+
+
+                        <div className='rouletteMain_box_wrap rouletteMain_P8_N8 luckyfortune'>
+                            <ul className="rouletteMain_box_part rouletteMain_box_part-8 luckyfortune">
+                                <li className="rouletteMain_box_part_list">
+                                    <div class="rouletteMain_box_part_color"></div>
+                                    <span class="rouletteMain_box_part_award">₹3,888</span>
+                                </li>
+                                <li className="rouletteMain_box_part_list">
+                                    <div class="rouletteMain_box_part_color"></div>
+                                    <span class="rouletteMain_box_part_award">₹888</span>
+                                </li>
+                                <li className="rouletteMain_box_part_list">
+                                    <div class="rouletteMain_box_part_color"></div>
+                                    <span class="rouletteMain_box_part_award">₹688</span>
+                                </li>
+                                <li className="rouletteMain_box_part_list">
+                                    <div class="rouletteMain_box_part_color"></div>
+                                    <span class="rouletteMain_box_part_award">₹188</span>
+                                </li>
+                                <li className="rouletteMain_box_part_list">
+                                    <div class="rouletteMain_box_part_color"></div>
+                                    <span class="rouletteMain_box_part_award">₹88</span>
+                                </li>
+                                <li className="rouletteMain_box_part_list">
+                                    <div class="rouletteMain_box_part_color"></div>
+                                    <span class="rouletteMain_box_part_award">₹68</span>
+                                </li>
+                                <li className="rouletteMain_box_part_list">
+                                    <div class="rouletteMain_box_part_color"></div>
+                                    <span class="rouletteMain_box_part_award">₹28</span>
+                                </li>
+                                <li className="rouletteMain_box_part_list">
+                                    <div class="rouletteMain_box_part_color"></div>
+                                    <span class="rouletteMain_box_part_award">₹10</span>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="rouletteMain_box_coin">
+                            <span class="rouletteMain_box_coinWrap"><img src="https://jiliwin.9terawolf.com/images/wof/1.png" class="rouletteMain_box_coin_1" /></span><span class="rouletteMain_box_coinWrap"><img src="https://jiliwin.9terawolf.com/images/wof/2.png" class="rouletteMain_box_coin_2" /></span><span class="rouletteMain_box_coinWrap"><img src="https://jiliwin.9terawolf.com/images/wof/3.png" class="rouletteMain_box_coin_3" /></span><span class="rouletteMain_box_coinWrap"><img src="https://jiliwin.9terawolf.com/images/wof/4.png" class="rouletteMain_box_coin_4" /></span><span class="rouletteMain_box_coinWrap"><img src="https://jiliwin.9terawolf.com/images/wof/5.png" class="rouletteMain_box_coin_5" /></span><span class="rouletteMain_box_coinWrap"><img src="https://jiliwin.9terawolf.com/images/wof/6.png" class="rouletteMain_box_coin_6" /></span>
+                        </div>
+                        <div class="rouletteMain_box_btn"></div>
+                        <div class="rouletteMain_box_numberTimes">
+                            <div>
+                                <div>
+                                    <span>0</span>
+                                    <span>coins</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="rouletteMain_winner hidden md:block">
+                        <div class="rouletteMain_winner_wrap" >
+                            <div class="rouletteMain_winner_head winnerList" >
+                                <div class="rouletteMain_winner_head_item" >Winner's List</div>
+                                <div class="rouletteMain_winner_head_item" >Winning Record</div>
+                                <div class="rouletteMain_winner_head_cursor leftMost" ></div>
+                            </div>
+                            <div class="rouletteMain_winner_slider">
+                                <ul class="rouletteMain_winner_list pl-0">
+                                    <li class="rouletteMain_winner_item">
+                                        <span class="rouletteMain_winner_date">2026-07-21</span>
+                                        <span>sha***</span>
+                                        <span class="rouletteMain_winner_prize">10.00</span>
+                                    </li>
+                                    <li class="rouletteMain_winner_item">
+                                        <span class="rouletteMain_winner_date">2026-07-21</span>
+                                        <span>sof***</span>
+                                        <span class="rouletteMain_winner_prize">10.00</span>
+                                    </li>
+                                    <li class="rouletteMain_winner_item">
+                                        <span class="rouletteMain_winner_date">2026-07-21</span>
+                                        <span>seret*****</span>
+                                        <span class="rouletteMain_winner_prize">28.00</span>
+                                    </li>
+                                    <li class="rouletteMain_winner_item">
+                                        <span class="rouletteMain_winner_date">2026-07-21</span>
+                                        <span>tmg12k******</span>
+                                        <span class="rouletteMain_winner_prize">10.00</span>
+                                    </li>
+                                    <li class="rouletteMain_winner_item">
+                                        <span class="rouletteMain_winner_date">2026-07-21</span>
+                                        <span>bac***</span>
+                                        <span class="rouletteMain_winner_prize">10.00</span>
+                                    </li>
+                                    <li class="rouletteMain_winner_item">
+                                        <span class="rouletteMain_winner_date">2026-07-21</span>
+                                        <span>mdas****</span>
+                                        <span class="rouletteMain_winner_prize">10.00</span>
+                                    </li>
+                                    <li class="rouletteMain_winner_item">
+                                        <span class="rouletteMain_winner_date">2026-07-21</span>
+                                        <span>kmra****</span>
+                                        <span class="rouletteMain_winner_prize">68.00</span>
+                                    </li>
+                                    <li class="rouletteMain_winner_item">
+                                        <span class="rouletteMain_winner_date">2026-07-21</span>
+                                        <span>mdakas******</span>
+                                        <span class="rouletteMain_winner_prize">10.00</span>
+                                    </li>
+                                    <li class="rouletteMain_winner_item">
+                                        <span class="rouletteMain_winner_date">2026-07-21</span>
+                                        <span>sori****</span>
+                                        <span class="rouletteMain_winner_prize">10.00</span>
+                                    </li>
+                                    <li class="rouletteMain_winner_item">
+                                        <span class="rouletteMain_winner_date">2026-07-21</span>
+                                        <span>kmra****</span>
+                                        <span class="rouletteMain_winner_prize">10.00</span>
+                                    </li>
+                                    <li class="rouletteMain_winner_item">
+                                        <span class="rouletteMain_winner_date">2026-07-21</span>
+                                        <span>kmra****</span>
+                                        <span class="rouletteMain_winner_prize">10.00</span>
+                                    </li><li class="rouletteMain_winner_item">
+                                        <span class="rouletteMain_winner_date">2026-07-21</span>
+                                        <span>kmra****</span>
+                                        <span class="rouletteMain_winner_prize">10.00</span>
+                                    </li><li class="rouletteMain_winner_item">
+                                        <span class="rouletteMain_winner_date">2026-07-21</span>
+                                        <span>kmra****</span>
+                                        <span class="rouletteMain_winner_prize">10.00</span>
+                                    </li><li class="rouletteMain_winner_item">
+                                        <span class="rouletteMain_winner_date">2026-07-21</span>
+                                        <span>kmra****</span>
+                                        <span class="rouletteMain_winner_prize">10.00</span>
+                                    </li><li class="rouletteMain_winner_item">
+                                        <span class="rouletteMain_winner_date">2026-07-21</span>
+                                        <span>kmra****</span>
+                                        <span class="rouletteMain_winner_prize">10.00</span>
+                                    </li><li class="rouletteMain_winner_item">
+                                        <span class="rouletteMain_winner_date">2026-07-21</span>
+                                        <span>kmra****</span>
+                                        <span class="rouletteMain_winner_prize">10.00</span>
+                                    </li><li class="rouletteMain_winner_item">
+                                        <span class="rouletteMain_winner_date">2026-07-21</span>
+                                        <span>kmra****</span>
+                                        <span class="rouletteMain_winner_prize">10.00</span>
+                                    </li><li class="rouletteMain_winner_item">
+                                        <span class="rouletteMain_winner_date">2026-07-21</span>
+                                        <span>kmra****</span>
+                                        <span class="rouletteMain_winner_prize">10.00</span>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default LuckySpin;
